c3ffc324ce8709f306d4a17b4b6d100a7766109d
mr xue <falethe@gmail.com>
2023-09-25 20:56:21 +0800

    main

--- src/views/Home/components/author.vue
+++ src/views/Home/components/author.vue
@@ -6,6 +6,12 @@ const activeName = ref('first')
 const handleClick = (tab, ) => {
   console.log(tab, event)
 }
+const value1 = ref(true)
+const value2 = ref(true)
+const value3 = ref(true)
+const value4 = ref(false)
+const value5 = ref(false)
+const value6 = ref(false)
 </script>
 
 <template>
@@ -15,8 +21,29 @@ const handleClick = (tab, ) => {
         </el-breadcrumb>
         <el-card>
             <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-                <el-tab-pane label="管理员" name="first">管理员</el-tab-pane>
-                <el-tab-pane label="普通用户" name="second">普通用户</el-tab-pane>
+                <el-tab-pane label="管理员" name="first">
+                    <el-row gutter="3">
+                        <el-col :span="6"><el-switch v-model="value1"  inactive-text="添加用户"/></el-col>
+                    </el-row>
+                    <el-row gutter="3">
+                        <el-col :span="6"><el-switch v-model="value2"  inactive-text="删除用户"/></el-col>
+                    </el-row>
+                    <el-row gutter="3">
+                        <el-col :span="6"><el-switch v-model="value3"  inactive-text="编辑文章"/></el-col>
+                    </el-row>
+                   
+                </el-tab-pane>
+                <el-tab-pane label="普通用户" name="second">
+                    <el-row gutter="3">
+                        <el-col :span="6"><el-switch v-model="value4"  inactive-text="添加用户"/></el-col>
+                    </el-row>
+                    <el-row gutter="3">
+                        <el-col :span="6"><el-switch v-model="value5"  inactive-text="删除用户"/></el-col>
+                    </el-row>
+                    <el-row gutter="3">
+                        <el-col :span="6"><el-switch v-model="value6"  inactive-text="编辑文章"/></el-col>
+                    </el-row>
+                </el-tab-pane>
             </el-tabs>
         </el-card>
 </template>
@@ -27,5 +54,9 @@ const handleClick = (tab, ) => {
 }
 .el-card{
     height: 800px;
+}
+.el-col{
+    display: flex;
+    align-items: center;
 }
 </style>
